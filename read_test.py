35120c03e38385c5ca8865aad047b6d4f628b8f8
rogerluo410 <rogerluo410@gmail.com>
2014-06-23 08:11:01 +0800

    commit

--- read_test.py
+++ read_test.py
@@ -15,12 +15,12 @@ def main():
   test =  re.search('([\(\+])?([0-9]{1,3}([\s])?)?([\+|\(|\-|\)|\s])?([0-9]{2,4})([\-|\)|\.|\s]([\s])?)?([0-9]{2,4})?([\.|\-|\s])?([0-9]{4,8})?([\.|\-|\s])?([0-9]{3,8})',str1)
   print test.group()
  
-  str2 = 'Personal Information: Name: Lei Xia Gender: Male Working Experience: 1 years'
+  str2 = 'Personal Information: Name:                       Yang Jialin   Gender: Male Working Experience: 1 years'
   #for i in range(0,len(str2)):
        
   test2 = re.search('(name|Name|NAME)+',str2)
   print test2.group()
-  test1 = re.search('(name|Name|NAME)+([\:|\s])+([A-Z]{1}[a-z]+\s{1})([A-Z]{1}[a-z]+)',str2)
+  test1 = re.search('(name|Name|NAME)+([\:|\s|\,]{1,})+([A-Z]{1}[A-Za-z]+\s{1})([A-Z]{1}[A-Za-z]+)?',str2)
   print test1.group()
 
 
